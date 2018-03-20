9f7c6e6aa54e962c0318c332e443c47c6c7ee407
Gangyi Sun <gangyi.sun@wustl.edu>
2018-03-19 19:47:11 -0500

    Created DevelopmentFile, loaded libraries and setwd

---
+++ DevelopmentFile.R
@@ -0,0 +1,13 @@
+## Applied Statistical Programing 
+## Midterm Exam
+## Gangyi Sun (441748)
+
+## Load libraries
+library(devtools)
+library(roxygen2)
+library(testthat)
+
+## Set working directory
+setwd("~/Documents/GitHub/Midterm")
+
+## Begin writing functions 
