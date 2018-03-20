4e58ab5e5f9fbcea159607ef6cef1ab5692341c0
Gangyi Sun <gangyi.sun@wustl.edu>
2018-03-19 19:55:02 -0500

    Defined Rasch class, wrote initialization method, created sample Rasch object

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -11,3 +11,32 @@ library(testthat)
 setwd("~/Documents/GitHub/Midterm")
 
 ## Begin writing functions 
+# Define S4 class Rasch
+setClass(Class="Rasch",
+         representation = representation(
+           testTakerName = "character",
+           a = "numeric",
+           y = "numeric"
+         ),
+         prototype = prototype(
+           testTakerName = c(),
+           a = c(),
+           y = c()
+         )
+)
+
+# initialization function
+setMethod("initialize", "Rasch",
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          }
+)
+
+# Sample code to test that methods work 
+qDiff<-c(1,2,3,4,5)
+qAns<-c(1,1,0,1,0)
+testRasch<-new("Rasch", testTakerName="Jim", a=qDiff, y=qAns)
+
+
+
